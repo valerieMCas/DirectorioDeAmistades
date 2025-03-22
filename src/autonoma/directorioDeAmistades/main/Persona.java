abc5a8a8466016a2bf7b8eccc0541b50249672ab
Martin <martintabordaaristizabal@gmail.com>
2025-03-22 17:37:26 -0500

    Se agrega atributos,constructor,getters, de la clase Persona

--- src/autonoma/directorioDeAmistades/main/Persona.java
+++ src/autonoma/directorioDeAmistades/main/Persona.java
@@ -6,8 +6,34 @@ package autonoma.directorioDeAmistades.main;
 
 /**
  *
- * @author usuario
+ * @author marti
  */
 public class Persona {
+    //Atributos
+    private String nombre;
+    private String telefono;
+    private String correo;
+
+    //Contructor
+    public Persona(String nombre, String telefono, String correo) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+    // Getters
+    
+    // Devuelve el nombre del amigo
+    public String getNombre() {
+        return nombre;
+    }
+    // Devuelve el teléfono del amigo
+    public String getTelefono() {
+        return telefono;
+    }
+    // Devuelve el correo electrónico del amigo
+    public String getCorreo() {
+        return correo;
+    }
+    
     
 }
